a91ea43e4ded77f41c22e5cad433c47620044789
KimChangYup <cykimh@gmail.com>
2021-06-19 17:37:02 +0900

    - AppConfig -> Bean 사용

--- Spring-Core-master/src/main/java/hello/core/AppConfig.java
+++ Spring-Core-master/src/main/java/hello/core/AppConfig.java
@@ -6,25 +6,32 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구성정보
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(getMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     // 역할이 다 들어난다
     // 이 코드만 바꾸면 된다
-    private MemberRepository getMemberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할이 들어난다
+    @Bean
     public DiscountPolicy discountPolicy() {
 //         return new FixDiscountPolicy();
         return new RateDiscountPolicy();
