e552f6e54c8349ff4f5dc8c8fc72f28bac8a0c22
KimChangYup <cykimh@gmail.com>
2021-06-19 16:51:22 +0900

    - AppConfig 리팩토링

--- Spring-Core-master/src/main/java/hello/core/AppConfig.java
+++ Spring-Core-master/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -7,14 +8,25 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+// 구성정보
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    // 역할이 다 들어난다
+    // 이 코드만 바꾸면 된다
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
+    // 역할이 들어난다
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
